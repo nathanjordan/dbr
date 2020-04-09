206088e4e13b499d120ad3d58eca4c68c5f74b9c
Xiaobing Li <xbli@uber.com>
2020-04-09 11:12:50 -0700

    cr

--- dialect/clickhouse.go
+++ dialect/clickhouse.go
@@ -1,8 +1,6 @@
 package dialect
 
 import (
-	"fmt"
-	"strings"
 	"time"
 )
 
@@ -11,56 +9,25 @@ import (
 type clickhouse struct{}
 
 func (d clickhouse) QuoteIdent(s string) string {
-	return quoteIdent(s, "`")
+	// Following the coding convention in this repo to call
+	// methods on base object directly.
+	return MySQL.QuoteIdent(s)
 }
 
 func (d clickhouse) EncodeString(s string) string {
-	var buf strings.Builder
-
-	buf.WriteRune('\'')
-	// https://dev.mysql.com/doc/refman/5.7/en/string-literals.html
-	for i := 0; i < len(s); i++ {
-		switch s[i] {
-		case 0:
-			buf.WriteString(`\0`)
-		case '\'':
-			buf.WriteString(`\'`)
-		case '"':
-			buf.WriteString(`\"`)
-		case '\b':
-			buf.WriteString(`\b`)
-		case '\n':
-			buf.WriteString(`\n`)
-		case '\r':
-			buf.WriteString(`\r`)
-		case '\t':
-			buf.WriteString(`\t`)
-		case 26:
-			buf.WriteString(`\Z`)
-		case '\\':
-			buf.WriteString(`\\`)
-		default:
-			buf.WriteByte(s[i])
-		}
-	}
-
-	buf.WriteRune('\'')
-	return buf.String()
+	return MySQL.EncodeString(s)
 }
 
 func (d clickhouse) EncodeBool(b bool) string {
-	if b {
-		return "1"
-	}
-	return "0"
+	return MySQL.EncodeBool(b)
 }
 
 func (d clickhouse) EncodeTime(t time.Time) string {
-	return `'` + t.UTC().Format(timeFormat) + `'`
+	return MySQL.EncodeTime(t)
 }
 
 func (d clickhouse) EncodeBytes(b []byte) string {
-	return fmt.Sprintf(`0x%x`, b)
+	return MySQL.EncodeBytes(b)
 }
 
 func (d clickhouse) EncodeArrayBegin() string {
@@ -71,6 +38,6 @@ func (d clickhouse) EncodeArrayEnd() string {
 	return "]"
 }
 
-func (d clickhouse) Placeholder(_ int) string {
-	return "?"
+func (d clickhouse) Placeholder(p int) string {
+	return MySQL.Placeholder(p)
 }
